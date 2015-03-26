2a7a926bcdfada313fc2d015ff3062397cd260a9
gcmgomes <guilherme.cm.gomes@gmail.com>
2015-03-25 21:02:43 -0300

    Fully defined the util::FileManager class

--- util/file_manager.h
+++ util/file_manager.h
@@ -3,10 +3,31 @@
 
 #include <iostream>
 #include <fstream>
+#include <priority_queue>
+#include <vector>
+#include <string>
+#include "tuple.h"
+
+namespace util {
 
 class FileManager {
   public:
-    FileManager(unsigned file_count, )
+    FileManager(unsigned file_count, std::string file_prefix);
+    
+    // Get next tuple from the file |file_prefix_| + |tuple_file_id| at the position
+    // |tuple_id|. Returns nullptr if EOF has already been reached, otherwise the caller
+    // takes ownership of the returned object.
+    Tuple* GetNextTuple(unsigned tuple_file_id, unsigned tuple_id);
+    
+    // Initializes |heap| with exactly 1 Tuple from each one of the |file_count_| files.
+    void InitializeHeap(std::priority_queue<Tuple, vector<Tuple>, &Tuple::LessThen>* heap);
+    
+  private:
+    unsigned file_count_;
+    std::string file_prefix_;
+    std::ifstream input_file_;
 };
+
+} //namespace util 
 
 #endif
