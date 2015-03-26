d6fd5adc8c6a74cf77e331325212e3b3dc5b9888
gcmgomes <guilherme.cm.gomes@gmail.com>
2015-03-25 21:42:40 -0300

    Implementing util::FileManager

--- util/file_manager.cc
+++ util/file_manager.cc
@@ -1,27 +1,52 @@
-#include "tuple.h"
+#include <cstring>
+#include <cstdio>
+#include "file_manager.h"
 
-// Returns true iff |a| is strictly smaller then |b|.
-const bool Tuple::LessThen(const Tuple& a, const Tuple& b) const {
-  if (a.term < b.term) {
-    return true;
-  }
+namespace util {
 
-  if (a.term > b.term) {
-    return false;
-  }
-
-  // a.term == b.term, compare document index.
-  if (a.document < b.document) {
-    return true;
-  }
-  if (a.document > b.document) {
-    return false;
-  }
-
-  // a.document == b.document, compare position (frequencies are ==).
-  if (a.position < b.position) {
-    return true;
-  }
-  // |a| is not strictly smaller then |b|.
-  return false;
+FileManager::FileManager(unsigned file_count, std::string file_prefix) {
+  file_count_ = file_count;
+  file_prefix_ = file_prefix;
 }
+
+Tuple* FileManager::GetNextTuple(unsigned tuple_file_id, unsigned tuple_id) {
+  if(tuple_file_id != input_file_id_) {
+    input_file_id_ = tuple_file_id;
+    input_file_.close();
+    
+    char file_id[16];
+    sprintf(file_id, "%u", tuple_file_id);
+    
+    std::string file_path = file_prefix_+file_id;
+    input_file_.open(file_path, std::ifstream::binary);
+    
+    // Each tuple is written as 4 unsigned (4 bytes each) values.
+    // Therefore, the n-th tuple starts at the (4*4*n)-byte.
+    input_file_.seekg(4*4*tuple_id);
+  }
+  
+  if(ifstream.eof() || ifstream.peek() == EOF) {
+    return nullptr;
+  }
+  
+  Tuple* tuple = new Tuple();
+  input_file_.read((char*) *tuple, 4*sizeof(unsigned));
+  tuple.tuple_file_id = input_file_id_;
+  tuple.next_tuple_id = tuple_id+1;
+  
+  return tuple;
+}
+    
+void FileManager::InitializeHeap(std::priority_queue<Tuple, vector<Tuple>, &Tuple::LessThen>* heap) {
+  unsigned file_id = file_count_-1;
+  while(i) {
+    Tuple* tuple = this->GetNextTuple(file_id, 0);
+    if(tuple != nullptr) {
+      heap->push_back(*tuple);
+      delete tuple;
+    }
+    i--;
+  }
+}
+
+} //namespace util
