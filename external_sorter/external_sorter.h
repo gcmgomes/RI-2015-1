d4da92cd466d57492694d6f48f25f8c66d111459
gcmgomes <guilherme.cm.gomes@gmail.com>
2015-03-26 21:21:35 -0300

    Update external_sorter.h

--- external_sorter/external_sorter.h
+++ external_sorter/external_sorter.h
@@ -5,22 +5,25 @@
 #include <priority_queue>
 #include <vector>
 #include "../util/tuple.h"
+#include "../util/file_manager.h"
+
+namespace sorting {
 
 class ExternalSorter {
  public:
-  ExternalSorter(unsigned max_tuple_buffer_size, unsigned max_heap_size);
+ // Takes ownership of |file_manager|.
+  ExternalSorter(util::FileManager* file_manager);
 
   ~ExternalSorter();
+  
  private:
-  // Maximum number of tuples to be stored in memory before being flushed.
-  unsigned max_tuple_buffer_size_;
-  // Array of tuples to be flushed when max_tuple_buffer_size_ is reached.
-  std::vector<Touple> tuple_buffer_;
 
-  // Maximum number of tuples contained that sorting_heap_ can hold.
-  unsigned max_heap_size_;
+  // Manager for file operations.
+  util::FileManager* file_manager_;
+  
   // Heap of Tuples that will be sorted.
-  std::priority_queue<Tuple, std::vector<Tuple>, &Tuple::LessThen> heap_;
+  std::priority_queue<util::Tuple, std::vector<util::Tuple>, &util::Tuple::LessThen> heap_;
 };
 
+} // namespace sorting
 #endif
