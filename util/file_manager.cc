257d07e87491e4c9784758098f8dbf24fbe25307
gcmgomes <guilherme.cm.gomes@gmail.com>
2015-03-26 21:47:26 -0300

    Update file_manager.cc

--- util/file_manager.cc
+++ util/file_manager.cc
@@ -4,9 +4,15 @@
 
 namespace util {
 
-FileManager::FileManager(unsigned file_count, std::string file_prefix) {
+FileManager::FileManager(unsigned file_count, std::string file_prefix, std::string output_file_path) {
   file_count_ = file_count;
   file_prefix_ = file_prefix;
+  output_file_.open(output_file_path.c_str(), std::ofstream::binary);
+}
+
+FileManager::~FileManager() {
+  input_file_.close();
+  output_file_.close();
 }
 
 Tuple* FileManager::GetNextTuple(unsigned tuple_file_id, unsigned tuple_id) {
@@ -17,8 +23,8 @@ Tuple* FileManager::GetNextTuple(unsigned tuple_file_id, unsigned tuple_id) {
     char file_id[16];
     sprintf(file_id, "%u", tuple_file_id);
     
-    std::string file_path = file_prefix_+file_id;
-    input_file_.open(file_path, std::ifstream::binary);
+    const std::string file_path = file_prefix_ + file_id;
+    input_file_.open(file_path.c_str(), std::ifstream::binary);
     
     // Each tuple is written as 4 unsigned (4 bytes each) values.
     // Therefore, the n-th tuple starts at the (4*4*n)-byte.
@@ -36,7 +42,12 @@ Tuple* FileManager::GetNextTuple(unsigned tuple_file_id, unsigned tuple_id) {
   
   return tuple;
 }
-    
+
+void OutputTuple(Tuple* tuple) {
+  // Writes the first 4 unsigned fields of |tuple| to |output_file_|.
+  output_file_.write((char*) *tuple,  4*sizeof(unsigned))
+}
+
 void FileManager::InitializeHeap(std::priority_queue<Tuple, vector<Tuple>, &Tuple::LessThen>* heap) {
   unsigned file_id = file_count_-1;
   while(i) {
@@ -49,4 +60,4 @@ void FileManager::InitializeHeap(std::priority_queue<Tuple, vector<Tuple>, &Tupl
   }
 }
 
-} //namespace util
+} // namespace util
