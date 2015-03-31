ca356ab6cc4927107e27a4abe1f93269f7dc54eb
gcmgomes <guilherme.cm.gomes@gmail.com>
2015-03-30 21:00:56 -0300

    Update vocabulary.cc

--- util/vocabulary.cc
+++ util/vocabulary.cc
@@ -20,17 +20,32 @@ bool Vocabulary::Check(const std::string& s) {
   return vocabulary_.count(s);
 }
 
-void Dump(const std::string& file_path) {
+void Vocabulary::Dump(const std::string& file_path) {
   std::ofstream output_file;
   output_file.open(file_path.c_str(), std::ofstream::binary);
-  std::unordered_map::iterator i = vocabulary_,begin();
+  auto i = vocabulary_,begin();
   while(i != vocabulary_.end()) {
     // Writes the amount of bytes present in the string.
-    output_file.write((char*) i->first.size(), 4*sizeof(size_t));
+    output_file.write(reinterprete_cast<const char*>(&i->first.size()), sizeof(unsigned));
     // Writes the string itself.
     output_file.write(i->first.c_str(), i->first.size());
     // Writes the term_id of the current string.
-    output_file.write((char*) i->second, 4*sizeof(unsigned));
+    output_file.write(reinterprete_cast<const char*>(&i->second), sizeof(unsigned));
     i++;
+  }
+}
+
+void Vocabulary::Load(const std::string& file_path) {
+  std::ifstream input_file;
+  input_file.open(file_path.c_str(), std::ifstream::binary);
+  while(!input_file.eof()) {
+    // Reads the amount of bytes present in the string.
+    unsigned size = 0;
+    input_file.read(sizeof(unsigned), reinterprete_cast<const char*>(&size));
+    // Reads the string itself.
+    char* str = new char[size];
+    input_file.read(size, str);
+    std::string key = str;
+    this->Insert(key);
   }
 }
