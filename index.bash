efc778324d854d3dbbf163654c2754fd774b8ddd
Guilherme Gomes <guilherme.cm.gomes@gmail.com>
2015-04-27 00:10:38 -0300

    index.bash fix

--- index.bash
+++ index.bash
@@ -7,16 +7,16 @@ collection_index=$2
 
 # Parse the collection.
 echo "collection_parser $1 $2 parser/stop_words.txt data/terms.txt data/tuples.bin"
-${ROOTDIR}/parser/bin/collection_parser $1 $2 parser/stop_words.txt data/terms.txt data/tuples.bin
+${ROOTDIR}/parser/bin/collection_parser $1 $2 ${ROOTDIR}/parser/stop_words.txt ${ROOTDIR}/data/terms.txt ${ROOTDIR}/data/tuples.bin
 
 # Get optimal number of runs.
 echo "optimal_run_counter data/tuples.bin 40000000"
-RUNCOUNT=$(${ROOTDIR}/util/bin/optimal_run_counter data/tuples.bin 40000000)
+RUNCOUNT=$(${ROOTDIR}/util/bin/optimal_run_counter ${ROOTDIR}/data/tuples.bin 40000000)
 
 # Partially sort the runs.
 echo "emplace_partial_sorter ${RUNCOUNT} data/tuples.bin"
-${ROOTDIR}/external_sorter/bin/emplace_partial_sorter ${RUNCOUNT} data/tuples.bin
+${ROOTDIR}/external_sorter/bin/emplace_partial_sorter ${RUNCOUNT} ${ROOTDIR}/data/tuples.bin
 
 # Perform multiway merging and generate the index.
 echo "external_sorter ${RUNCOUNT} data/tuples.bin data/index.bin"
-${ROOTDIR}/external_sorter/bin/external_sorter ${RUNCOUNT} data/tuples.bin data/index.bin
+${ROOTDIR}/external_sorter/bin/external_sorter ${RUNCOUNT} ${ROOTDIR}/data/tuples.bin ${ROOTDIR}/data/index.bin
